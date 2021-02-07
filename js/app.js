0ba0fe62e87e4520363102b0e195a8c893229d8d
anwarshraideh <ahshraida@gmail.com>
2021-02-07 15:36:50 +0200

    i did some changes in js file

--- js/app.js
+++ js/app.js
@@ -3,47 +3,55 @@ console.log('class 04');
 
 var productName = prompt(" what is the product name ");
 var degree=  prompt(" what is the degree of the product "); 
-//var isExisting =prompt(" it is the product  existing ");
+var isExisting =prompt(" it is the product  existing ");
 
-var isExisting=true;
+
 
 console.log ('Product Name :' + productName + ', ' + 'Degree : ' + degree +' , ' + 'Product is Existing : ' + isExisting  );
 
+document.write('<p>Product Name :' + productName + ', ' + 'Degree : ' + degree +' , ' + 'Product is Existing : ' + isExisting +'. </p> ' );
 
-/*if (isExisting ==false)
+
+/*if (isExisting == false)
 {
 
 console.log ('the product not existing');
+document.write("<h1>the product not existing</h1>");
 
-} else(isExisting ==true)
- {
+
+} else {
 
  console.log ('the product existing');
+ document.write("<h1>the product existing</h1>");
+
 
  } */
 
  if (degree >=15 && degree <= 25 )
  {
   
-    console.log('product is Existing  ');
+    console.log('this degree is available ');
+    document.write("<h3>this degree is available</h3>");
 
  }
  else
  {
 
-    console.log('product is sold out ');
+    console.log('this degree is sold out ');
+    document.write("<h4>this degree is sold out </h4>");
+
 
 
  }
 
-var text;
+/* var text;
 var isExisting = confirm("Press this confirm button!");
 if (isExisting == true) {
     text = "the product existing";
 } else {
     text = "the product not existing";
 }
- document.write('Product Name :' + productName + ', ' + 'Degree : ' + degree +' , ' + 'Product is Existing : ' + isExisting  );
+*/
 
 
 
