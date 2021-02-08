56d733f80c93fa6ab473b1a58bb1b0da31fadb5b
anwarshraideh <ahshraida@gmail.com>
2021-02-08 13:11:57 +0200

    adding changes on js file

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 
 console.log('class 04');
 
+var productKind= prompt(" what do you want foundation , powder , lipsteck : ");
 var productName = prompt(" what is the product name ");
+var numOfpiece = prompt("what is the number of piece");
 var degree=  prompt(" what is the degree of the product "); 
 var isExisting =prompt(" it is the product  existing ");
-
 
 
 console.log ('Product Name :' + productName + ', ' + 'Degree : ' + degree +' , ' + 'Product is Existing : ' + isExisting  );
@@ -18,14 +19,13 @@ document.write('<p>Product Name :' + productName + ', ' + 'Degree : ' + degree +
 console.log ('the product not existing');
 document.write("<h1>the product not existing</h1>");
 
-
 } else {
 
  console.log ('the product existing');
  document.write("<h1>the product existing</h1>");
 
-
  } */
+
 
  if (degree >=15 && degree <= 25 )
  {
@@ -40,18 +40,46 @@ document.write("<h1>the product not existing</h1>");
     console.log('this degree is sold out ');
     document.write("<h4>this degree is sold out </h4>");
 
-
-
  }
 
-/* var text;
+var text;
 var isExisting = confirm("Press this confirm button!");
 if (isExisting == true) {
     text = "the product existing";
 } else {
     text = "the product not existing";
 }
-*/
+
+
+while(productKind !=="foundation" && productKind !=="powder" && productKind !== "lipsteck")
+{
+    productKind= prompt(" what do you want foundation , powder , lipsteck : ");
+
+}
+
+var itemOrder ='';
+if (productKind == "foundation")
+{
+    itemOrder ='<img src="images/f.jpg"/>';
+}
+else if (productKind == "powder")
+{
+    itemOrder ='<img src="images/p.png"/>';
+}
+else if (productKind == "lipsteck")
+{
+    itemOrder ='<img src="images/l.jpg"/>';
+
+}
+
+var result ='';
+for ( var i= 0; i < numOfpiece ; i++) {
+
+    result = result + itemOrder;
+    
+}
+document.write(result);
+
 
 
 
